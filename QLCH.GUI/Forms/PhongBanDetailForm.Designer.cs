dd3efbb307f7c128c00bc405f6ca3905de76b69e
QuanHD0610 <quanhd@itp-link.com>
2025-05-24 18:54:01 +0700

    [FontEnd] Add ui phong ban and chuc vu detail form

---
+++ QLCH.GUI/Forms/PhongBanDetailForm.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace QLCH.GUI.Forms
+{
+    partial class PhongBanDetailForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.customTitle1 = new QLCH.GUI.Custom.CustomTitle();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.customLabel4 = new QLCH.GUI.Custom.CustomLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.customLabel2 = new QLCH.GUI.Custom.CustomLabel();
+            this.customLabel1 = new QLCH.GUI.Custom.CustomLabel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // customTitle1
+            // 
+            this.customTitle1.AutoSize = true;
+            this.customTitle1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold);
+            this.customTitle1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.customTitle1.Location = new System.Drawing.Point(169, 9);
+            this.customTitle1.Name = "customTitle1";
+            this.customTitle1.Size = new System.Drawing.Size(138, 46);
+            this.customTitle1.TabIndex = 0;
+            this.customTitle1.Text = "Tiêu đề";
+            this.customTitle1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.customLabel4);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.customLabel2);
+            this.panel1.Controls.Add(this.customLabel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 62);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(509, 495);
+            this.panel1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(242, 313);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Hủy";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(338, 313);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Lưu";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(177, 125);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(236, 153);
+            this.textBox3.TabIndex = 8;
+            // 
+            // customLabel4
+            // 
+            this.customLabel4.AutoSize = true;
+            this.customLabel4.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.customLabel4.ForeColor = System.Drawing.Color.DarkBlue;
+            this.customLabel4.Location = new System.Drawing.Point(25, 122);
+            this.customLabel4.Name = "customLabel4";
+            this.customLabel4.Size = new System.Drawing.Size(64, 28);
+            this.customLabel4.TabIndex = 7;
+            this.customLabel4.Text = "Mô tả";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(177, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // customLabel2
+            // 
+            this.customLabel2.AutoSize = true;
+            this.customLabel2.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.customLabel2.ForeColor = System.Drawing.Color.DarkBlue;
+            this.customLabel2.Location = new System.Drawing.Point(25, 74);
+            this.customLabel2.Name = "customLabel2";
+            this.customLabel2.Size = new System.Drawing.Size(142, 28);
+            this.customLabel2.TabIndex = 3;
+            this.customLabel2.Text = "Tên phòng ban";
+            // 
+            // customLabel1
+            // 
+            this.customLabel1.AutoSize = true;
+            this.customLabel1.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.customLabel1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.customLabel1.Location = new System.Drawing.Point(25, 28);
+            this.customLabel1.Name = "customLabel1";
+            this.customLabel1.Size = new System.Drawing.Size(141, 28);
+            this.customLabel1.TabIndex = 0;
+            this.customLabel1.Text = "Mã phòng ban";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.customTitle1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(509, 60);
+            this.panel2.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(177, 35);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(236, 22);
+            this.textBox2.TabIndex = 11;
+            // 
+            // PhongBanDetailForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 557);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "PhongBanDetailForm";
+            this.Text = "PhongBanDetailForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Custom.CustomTitle customTitle1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private Custom.CustomLabel customLabel4;
+        private System.Windows.Forms.TextBox textBox1;
+        private Custom.CustomLabel customLabel2;
+        private Custom.CustomLabel customLabel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
